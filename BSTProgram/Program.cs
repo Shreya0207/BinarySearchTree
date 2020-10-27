ad6f2b7f8ea91e77fcf3cdb8add8040f8e45d78e
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-27 20:09:37 +0530

    [Shreya]Add: displaying the welcome message

---
+++ BSTProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSTProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Binary Search Tree program");
+        }
+    }
+}
